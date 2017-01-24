47ae038117a9cfd2a6a66d1d48bd9fcd823f5121
Jacob Samro <JacobSamro@users.noreply.github.com>
2017-01-24 11:15:05 +0530

    Initial Web Version - FireFemy Server Edition

---
+++ script.js
@@ -0,0 +1,52 @@
+
+$("#calculate").click(function(){
+	
+	var reader = new FileReader();
+
+	try{
+
+
+
+	var file = image.files[0];
+
+	reader.onload = function (e) {
+
+		var _dataImage = e.target.result.replace("data:image/png;base64,","");
+		
+
+        $.ajax({
+		  method: "POST",
+		  url: "http://103.211.218.24/imagex",
+		  data: {
+		  		image : _dataImage
+		  }
+		})
+		.fail(function(msg){
+
+		    errorMessage("Internet Connection Failed!");
+
+		})
+		.done(function( msg ) {
+
+
+			$("#result").fadeIn(500);
+
+			msg = JSON.parse(msg);
+
+			var HTML = "Expression : " + msg.Expression + "<br/>" + "Result : " + msg.Result;
+
+			result.innerHTML = HTML;
+
+		});
+
+
+    }
+
+    reader.readAsDataURL(file);
+
+	}catch(e){
+
+		alert("Unable to load or process file");
+	}
+
+});
