1a55db5d7c435e2f71b2da52e77fe3150f0c8ea4
Jacob Samro <JacobSamro@users.noreply.github.com>
2017-01-24 11:44:50 +0530

    Camera option Implemented, Upload Icons changed

--- script.js
+++ script.js
@@ -50,3 +50,14 @@ $("#calculate").click(function(){
 	}
 
 });
+
+
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});
